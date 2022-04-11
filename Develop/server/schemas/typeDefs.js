2e2edecfd1a99210e7ef70604d59e597b67f0f61
Nicholas Nolen <nicnolen@ymail.com>
2022-04-11 13:37:37 -0500

    added typeDef for User mutation in typeDef.js

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -1,6 +1,12 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+  # Define User mutation
+  type User {
+    _id: ID
+    username: String
+    email: String
+  }
   # Define mutations
   type Mutation {
     login(email: String!, password: String!): User
