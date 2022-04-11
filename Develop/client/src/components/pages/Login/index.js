1dbae04fa7c37b02c1638223a746ef717e98925e
Nicholas Nolen <nicnolen@ymail.com>
2022-04-11 11:05:54 -0500

    fixed ReactDOM issue in index.js file

--- Develop/client/src/components/pages/Login/index.js
+++ Develop/client/src/components/pages/Login/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Login() {
   return (
-  <div>
-    <h2>Login</h2>
-  </div>
+    <div>
+      <Link to="signup">Signup</Link>
+
+      <h2>Login</h2>
+    </div>
   );
 }
 
