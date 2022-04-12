127d61c211b38f7c145b425f45541b999b56d04a
Nicholas Nolen <nicnolen@ymail.com>
2022-04-12 13:32:48 -0500

    added mutation to add users in mutations.js

--- Develop/client/src/utils/mutations.js
+++ Develop/client/src/utils/mutations.js
@@ -11,3 +11,14 @@ export const LOGIN_USER = gql`
     }
   }
 `;
+
+export const ADD_USER = gql`
+  mutation addUser($firstName: String!, $lastName: String!, $email: String!, $password: String!) {
+    addUser(firstName: $firstName, lastName: $lastName, email: $email, password: $password) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
