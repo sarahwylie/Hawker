275e8c12396dba04deba55b26d5e0c9d422982a0
Nicholas Nolen <nicnolen@ymail.com>
2022-04-12 10:40:06 -0500

    imported feature/App-layout and changed file structure

--- Develop/client/src/components/Footer/index.js
+++ Develop/client/src/components/Footer/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { BsInstagram } from 'react-icons/bs';
+import Year from '../utils/helpers';
 
 function Footer() {
-  return <div></div>;
+    return (
+        <section className='flex-row footer'>
+            <div className='foot col-3'>
+            ©<span><Year /></span>by Team Hawk
+            </div>
+            <div className='foot col-3'>
+                <a href="https://www.instagram.com/moolovesdonuts/"
+                    alt="instagram"
+                    target="_blank"
+                    rel="noreferrer"><BsInstagram /></a></div>
+        </section>
+    )
 }
 
 export default Footer;
