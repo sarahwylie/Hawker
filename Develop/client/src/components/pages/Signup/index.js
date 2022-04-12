08ac58212638c7cae67aedb321d4ee62410bc827
Nicholas Nolen <nicnolen@ymail.com>
2022-04-12 14:25:06 -0500

    added form submit handler and handleChange functions to the signup component

--- Develop/client/src/components/pages/Signup/index.js
+++ Develop/client/src/components/pages/Signup/index.js
@@ -10,6 +10,28 @@ function Signup() {
   const [formState, setFormState] = useState({ email: '', password: '' });
   const [addUser] = useMutation(ADD_USER);
 
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    const mutationResponse = await addUser({
+      variables: {
+        email: formState.email,
+        password: formState.password,
+        firstName: formState.firstName,
+        lastName: formState.lastName
+      }
+    });
+    const token = mutationResponse.data.addUser.token;
+    Auth.login(token);
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value
+    });
+  };
+
   return (
     <div>
       <h2>Signup</h2>
