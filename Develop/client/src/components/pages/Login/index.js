fdb858c5df39de208f68932d2c07572963e86e97
sammychvz <sammy_chvz17@outlook.com>
2022-04-11 22:34:13 -0500

    added the form for listing an item

--- Develop/client/src/components/pages/Login/index.js
+++ Develop/client/src/components/pages/Login/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Login() {
   return (
   <div>
-    <h2>Login</h2>
+
   </div>
   );
 }
--- Develop/client/src/components/pages/PostItem/index.js
+++ Develop/client/src/components/pages/PostItem/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
 function PostItem() {
-  return <div></div>;
+  return <div>
+    <div> 
+       <form>
+       <label for="itemImage">Insert Image </label>  
+       <input type="file" name="itemImage"></input>
+
+       <label for="itemTitle">Item Title</label>
+       <input type="text" name="itemTitle"placeholder="Title"></input>
+
+       <label for="price">Price</label>
+       <input type="text" name="price" placeholder="Price of Item"></input>
+
+       <label for="description">Description</label>
+       <input type="text" name="description" placeholder="Description"></input>
+
+       <input type="submit" value="List Item"></input>
+
+       </form>  
+    </div>  
+  </div>
 }
 
 export default PostItem;
