db1926ee471cd1eed41a913fec365f36f20f5e17
Nicholas Nolen <nicnolen@ymail.com>
2022-04-12 14:24:13 -0500

    imported dependencies to signup component and added state to the form using the useState hook and allowed the ADD_USER mutation to be used using the useMutation hook

--- Develop/client/src/components/pages/Login/index.js
+++ Develop/client/src/components/pages/Login/index.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../../../utils/mutations';
-import Auth from '../utils/auth';
+import Auth from '../../../utils/auth';
 import '../../../assets/css/Login.css';
 
 function Login() {
--- Develop/client/src/components/pages/Signup/index.js
+++ Develop/client/src/components/pages/Signup/index.js
@@ -1,8 +1,15 @@
 // Import dependencies
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
+import Auth from '../../../utils/auth';
 
 // Create Signup form page component
 function Signup() {
+  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [addUser] = useMutation(ADD_USER);
+
   return (
     <div>
       <h2>Signup</h2>
