da4f44ca8e7aef6e2765b7ba0e658cc1f29f7bef
sammychvz <sammy_chvz17@outlook.com>
2022-04-12 18:20:37 -0500

    merge conflict

--- Develop/client/src/components/pages/SingleItem/index.js
+++ Develop/client/src/components/pages/SingleItem/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 
 function SingleItem() {
-<<<<<<< HEAD
-  return <div>Single item</div>;
-=======
   return (
     <div>
       <h2>SingleItem</h2>
     </div>
   );
->>>>>>> fdf0f8051796db62dffa4c4f0342465bf0d4810b
 }
 
 export default SingleItem;
