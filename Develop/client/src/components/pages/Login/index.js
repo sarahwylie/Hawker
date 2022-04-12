a66846ccc6134d3134d6a34185b9c94037eb48ce
Nicholas Nolen <nicnolen@ymail.com>
2022-04-12 13:53:27 -0500

    added state to form state and allowed form to use mutations with useMutation hook

--- Develop/client/src/components/pages/Login/index.js
+++ Develop/client/src/components/pages/Login/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../../../utils/mutations';
+import Auth from '../utils/auth';
 import '../../../assets/css/Login.css';
 
 function Login() {
+  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [login, { error }] = useMutation(LOGIN_USER);
+
   return (
-  <div>
-    <h2>Login</h2>
-  </div>
+    <div>
+      <h2>Login</h2>
+    </div>
   );
 }
 
