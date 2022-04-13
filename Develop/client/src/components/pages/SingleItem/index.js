becbfebb3b52ce1d5342df9fd5b672430f17adb7
loch <nigelloch@gmail.com>
2022-04-13 18:10:14 -0500

    imports

--- Develop/client/src/components/pages/SingleItem/index.js
+++ Develop/client/src/components/pages/SingleItem/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Auth from '../../../utils/auth';
+import Hawker from '../../../assets/images/icons/Hawker.svg';
 
 function SingleItem() {
+
+  
   return (
     <div>
       <h2>SingleItem</h2>
