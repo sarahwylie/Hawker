363a3b012af1a44bd3cf0735a77e9b06aa558749
sarahwylie <sarahannemariawylie@gmail.com>
2022-04-14 14:28:44 -0500

    header done except for categories

--- Develop/client/src/App.js
+++ Develop/client/src/App.js
@@ -42,7 +42,7 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <Header />
+      <Header/>
       <Router>
         <>
           <Routes>
--- Develop/client/src/components/Header/index.js
+++ Develop/client/src/components/Header/index.js
@@ -1,27 +1,26 @@
 import React from 'react';
 import { NavDropdown } from 'react-bootstrap';
 import Hawker from '../../assets/images/icons/Hawker.svg';
-import { Link } from 'react-router-dom';
 import '../../assets/css/Header.css';
-// import Auth from '../../utils/auth';
+import Signup from '../pages/Signup'
+import Login from '../pages/Login'
 
-function Header({history}) {
-let user = JSON.parse(localStorage.getItem('user-info'))
-console.warn(user)
-// const history = useHistory();
-function logOut() {
-  localStorage.clear();
-  history.push('/')
-}
+function Header({ history }) {
+  let user = JSON.parse(localStorage.getItem('user-info'))
+  // console.warn(user)
+  function logOut() {
+    localStorage.clear();
+    history.push('/')
+  }
 
   return (
     <header className='head'>
-      <div>
-        <div>
-           
+      <div className="flex-row">
+        <div className='col-4'>
+
         </div>
-        <div>
-          <Link to="/" />
+        <div className='col-4'>
+          <a href="/">
             <span role="img" aria-label="img-name">
               <img
                 src={Hawker}
@@ -30,13 +29,29 @@ function logOut() {
                 id="logo"
               />
             </span>
-            {localStorage.getItem('user-info')?
-            <div>
-              <NavDropdown title={user && user.name}>
-              <NavDropdown.Item onClick={logOut}>Logout</NavDropdown.Item>
-              </NavDropdown>
-            </div>
-            :null}
+          </a>
+          <div className='col-4'>
+            {localStorage.getItem('user-info') ?
+              <div>
+                <a href="/orderHistory">
+                  Order History
+                </a>
+                <a href="/">
+                  <NavDropdown title={user && user.name}>
+                    <NavDropdown.Item onClick={logOut}>Logout</NavDropdown.Item>
+                  </NavDropdown>
+                </a>
+              </div>
+              :
+              <div>
+                <a href="/signup">
+                  <button onClick={<Signup />}>Signup</button>
+                </a>
+                <a href="/login">
+                  <button onClick={<Login />}>Login</button></a>
+              </div>
+            }
+          </div>
         </div>
       </div>
     </header>
