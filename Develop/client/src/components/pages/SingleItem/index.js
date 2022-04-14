173be953fffdb223f2a3c6284f61769669591ed7
loch <nigelloch@gmail.com>
2022-04-13 22:07:46 -0500

    added html structure

--- Develop/client/src/components/pages/SingleItem/index.js
+++ Develop/client/src/components/pages/SingleItem/index.js
@@ -2,15 +2,25 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Auth from '../../../utils/auth';
 import Hawker from '../../../assets/images/icons/Hawker.svg';
+import '../../../assets/css/Singleitem.css';
 
 function SingleItem() {
-
   
+
   return (
-    <div>
-      <h2>SingleItem</h2>
-    </div>
+    <body>
+        <div>
+          <h2>Single Item</h2>
+        </div>
+      <div>
+        <img src='../../../assets/images/tech/keyboard_mouse.jpg'></img>
+        <p>lorem itsum </p>
+      </div>
+      <button>Back to Items</button>
+      <button>Buy Me</button>
+    </body>
   );
+
 }
 
 export default SingleItem;
