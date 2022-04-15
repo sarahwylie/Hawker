31bfc7216a162e9e16eb8eeff3297d51dceda88b
loch <nigelloch@gmail.com>
2022-04-15 16:29:04 -0500

    adjusted html

--- Develop/client/src/components/pages/SingleItem/index.js
+++ Develop/client/src/components/pages/SingleItem/index.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Auth from '../../../utils/auth';
-import Hawker from '../../../assets/images/icons/Hawker.svg';
 import '../../../assets/css/Singleitem.css';
 import { useQuery } from '@apollo/client';
 import { QUERY_SINGLE_ITEM } from '../../../utils/queries';
+import imagesData from '../../pages/Homepage/imagesData.json';
 
 function SingleItemModal() {
   
-  { 
+  // { 
 
-  return (
-    <div className='modalBackdrop'>
-        <div className='modalContainer'>
-          <h2>Item Title</h2>
-              <p>{}</p>
-      <button type="button">Back to Items</button>
-      <button type="button">Buy Me</button>
-      </div>
-    </div>
-  );
-}
+  // return (
+  //   <div className='modalBackdrop'>
+  //       <div className='modalContainer'>
+  //         <h2>Item Title</h2>
+  //         {imagesData.map((image, i) => (
+  //           <><a href="http://localhost:3000" key={i}>
+  //             <div>
+  //               <img {...imagesData[i]} alt={imagesData[i].alt} width="25%" />
+  //               <p>{image.category}</p>
+  //             </div>
+  //           </a><div>
+  //               <button type="button">Back to Items</button>
+  //               <button type="button">Buy Me</button>
+  //             </div></>
+  //         </div>
+  //   </div>
+  // )};
+
 }
 
 export default SingleItemModal;
