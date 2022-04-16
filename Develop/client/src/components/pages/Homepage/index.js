bd75fcaf900c6a10d2f3038c4958e39bc28ef05e
CraigJensen <cmjensen82@gmail.com>
2022-04-16 09:02:49 -0500

    more styling of the cards

--- Develop/client/src/components/pages/Homepage/index.js
+++ Develop/client/src/components/pages/Homepage/index.js
@@ -8,13 +8,17 @@ function Homepage() {
   return (
     <div className="itemContainer">
       {imagesData.map((image, i) => (
-        <CCard  loading="lazy">
+        <CCard  loading="lazy" key={i}>
           <CCardImage orientation="top" {...imagesData[i]} alt={imagesData[i].alt} width="100%" />
           <CCardBody>
             <CCardTitle>{imagesData[i].alt}</CCardTitle>
             <CCardText>
               {imagesData[i].category}
             </CCardText>
+            <CCardText>
+            {imagesData[i].description}
+            </CCardText>
+            
             <CButton href="#">Purchase</CButton>
           </CCardBody>
         </CCard>
