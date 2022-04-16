6896026d3da4c42be8c583ff4b4ca316d60086b9
CraigJensen <cmjensen82@gmail.com>
2022-04-15 21:50:09 -0500

    added cards for homepage, adjusted sass files

--- Develop/client/src/components/pages/Homepage/index.js
+++ Develop/client/src/components/pages/Homepage/index.js
@@ -1,50 +1,26 @@
 import React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions } from '@mui/material';
+import { CCard, CCardImage, CCardBody, CCardTitle, CCardText, CButton } from '@coreui/react';
 
-// import imagesData from './imagesData.json';
+
+import imagesData from './imagesData.json';
 
 function Homepage() {
   return (
     <div>
-      {/* {imagesData.map((image, i) => ( */}
-        <Card sx={{ maxWidth: 345 }}>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            height="140"
-            image="../../../assets/images/auto/black_car.jpg"
-            alt="Black Car"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Black Car
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Stuff about the Black Car
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary">
-            Purchase
-          </Button>
-        </CardActions>
-      </Card>
-      {/* ))} */}
+      {imagesData.map((image, i) => (
+        <CCard style={{ width: '18rem' }}>
+          <CCardImage orientation="top" {...imagesData[i]} alt={imagesData[i].alt} width="100%" />
+          <CCardBody>
+            <CCardTitle>{imagesData[i].alt}</CCardTitle>
+            <CCardText>
+              {imagesData[i].category}
+            </CCardText>
+            <CButton href="#">Purchase</CButton>
+          </CCardBody>
+        </CCard>
+      ))}
     </div>
   );
 }
 
 export default Homepage;
-
-
-{/* <a href="http://localhost:3000" key={i}>
-          <div className="imageGrid">
-            <img {...imagesData[i]} alt={imagesData[i].alt} width="25%" />
-            <p>{image.category}</p>
-          </div>
-        </a> */}
