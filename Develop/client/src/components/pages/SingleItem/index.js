1a9d993b63a1825861c8500279040d90dc292b58
loch <nigelloch@gmail.com>
2022-04-16 22:59:30 -0500

    added price/quantity to item details

--- Develop/client/src/components/pages/SingleItem/index.js
+++ Develop/client/src/components/pages/SingleItem/index.js
@@ -1,4 +1,4 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Auth from '../../../utils/auth';
 import '../../../assets/css/Singleitem.css';
@@ -7,39 +7,45 @@ import { QUERY_SINGLE_ITEM } from '../../../utils/queries';
 import Homepage from '../Homepage';
 import { Modal, Button } from 'react-bootstrap';
 
-
 function SingleItemModal(props) {
-  const itemData = props
+  const itemData = props;
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-  let item = props.props
+  let item = props.props;
 
-    return (
-      <div>
-        {console.log(item)}
-      {/* {item ? ( */}
-          <><Button variant="primary" onClick={handleShow}>
-                  {item.title}
-              </Button><Modal show={show} centered onHide={handleClose}>
-                      <Modal.Header closeButton>
-                      <Modal.Title>{item.title}</Modal.Title>
-                      </Modal.Header>
-                      <Modal.Body>{item.description}</Modal.Body>
-                      <Modal.Footer>
-                          <Button variant="secondary" onClick={handleClose}>
-                              Back to Items
-                          </Button>
-                          <Button variant="primary" onClick={handleClose}>
-                              Buy Me
-                          </Button>
-                      </Modal.Footer>
-                  </Modal></>
-          </div>
-      
+  return (
+    <div>
+      <>
+        <Button variant="primary" onClick={handleShow}>
+          {item.title}
+        </Button>
+        <Modal show={show} centered onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>{item.title}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>{item.description}</Modal.Body>
+          <Modal.Footer>
+            <p>Price:</p>
+            <div>{item.price}</div>
+            <form>
+              <label>
+                Quantity:
+                <input type="text" name="name" />
+              </label>
+            </form>
+            <Button variant="secondary" onClick={handleClose}>
+              Back to Items
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Buy Me
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    </div>
   );
-
 }
 
 export default SingleItemModal;
