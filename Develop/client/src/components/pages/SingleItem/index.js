696a57b6d2aaf8d49a7b3a06302ee11afe66e8ce
JideOgun <JideOgun@github.com>
2022-04-18 15:54:59 -0500

    cleaned up the single item and home pages

--- Develop/client/src/components/pages/SingleItem/index.js
+++ Develop/client/src/components/pages/SingleItem/index.js
@@ -1,18 +1,17 @@
-import React, { useState } from 'react';
+
 import { Link } from 'react-router-dom';
-// import Auth from '../../../utils/auth';
+
 import '../../../assets/css/Singleitem.css';
 import { useQuery } from '@apollo/client';
 import { QUERY_SINGLE_ITEM } from '../../../utils/queries';
 import { CCard, CCardImage, CCardBody, CCardTitle, CCardText, CButton } from '@coreui/react';
-// import Homepage from '../Homepage';
-// import { Modal, Button } from 'react-bootstrap';
+
 
 function SingleItem() {
   // substring number probably will change we stop hosting on Local host
   let itemId = window.location.href.substring(33);
   console.log(itemId);
-  const { loading, error, data } = useQuery(QUERY_SINGLE_ITEM, { variables: { id: itemId } });
+  const {data } = useQuery(QUERY_SINGLE_ITEM, { variables: { id: itemId } });
   console.log(data);
 
   const getSingleItemData = () => {
@@ -37,44 +36,3 @@ function SingleItem() {
 }
 
 export default SingleItem;
-
-// function SingleItemModal(props) {
-//   const itemData = props;
-//   const [show, setShow] = useState(false);
-
-//   const handleClose = () => setShow(false);
-//   const handleShow = () => setShow(true);
-//   let item = props.props;
-
-//   return (
-//     <div>
-//       <>
-//         <Button variant="primary" onClick={handleShow}>
-
-//         </Button>
-//         <Modal show={show} centered onHide={handleClose}>
-//           <Modal.Header closeButton>
-//             <Modal.Title></Modal.Title>
-//           </Modal.Header>
-//           <Modal.Body></Modal.Body>
-//           <Modal.Footer>
-//             <p>Price:</p>
-//             <div></div>
-//             <form>
-//               <label>
-//                 Quantity:
-//                 <input type="text" name="name" />
-//               </label>
-//             </form>
-//             <Button variant="secondary" onClick={handleClose}>
-//               Back to Items
-//             </Button>
-//             <Button variant="primary" onClick={handleClose}>
-//               Buy Me
-//             </Button>
-//           </Modal.Footer>
-//         </Modal>
-//       </>
-//     </div>
-//   );
-// }
