2378f4d3f5d06bf4bfd52ed49b1b6b43fa954928
Nicholas Nolen <nicnolen@ymail.com>
2022-04-18 08:45:08 -0500

    wrote passing test for UPDATE_PRODUCTS

--- Develop/client/src/__tests__/reducers.test.js
+++ Develop/client/src/__tests__/reducers.test.js
@@ -1,5 +1,6 @@
 // import our actions
 import { UPDATE_PRODUCTS, UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../utils/actions';
+import { reducer } from '../utils/reducers';
 
 // create a sample of what our global state will look like
 const initialState = {
