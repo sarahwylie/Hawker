0401a07735d6279c14879b142bdce6e4cb4c43da
Nicholas Nolen <nicnolen@ymail.com>
2022-04-18 08:54:07 -0500

    added reducer for UPDATE_CATEGORIES and passed the test

--- Develop/client/src/utils/reducers.js
+++ Develop/client/src/utils/reducers.js
@@ -8,8 +8,13 @@ export const reducer = (state, action) => {
         ...state,
         products: [...action.products]
       };
+    // if action type value is the value of `UPDATE_CATEGORIES`, return a new state object with an updated categories array
+    case UPDATE_CATEGORIES:
+      return {
+        ...state,
+        categories: [...action.categories]
+      };
 
-    // if it's none of these actions, do not update state at all and keep things the same!
     default:
       return state;
   }
