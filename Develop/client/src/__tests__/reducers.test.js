6f8d9793ed928760d1e5565eafae0fe6ffeb2c30
Nicholas Nolen <nicnolen@ymail.com>
2022-04-18 08:51:16 -0500

    added test for UPDATE_CATEGORIES

--- Develop/client/src/__tests__/reducers.test.js
+++ Develop/client/src/__tests__/reducers.test.js
@@ -19,3 +19,13 @@ test('UPDATE_PRODUCTS', () => {
   expect(newState.products.length).toBe(2);
   expect(initialState.products.length).toBe(0);
 });
+
+test('UPDATE_CATEGORIES', () => {
+  let newState = reducer(initialState, {
+    type: UPDATE_CATEGORIES,
+    categories: [{}, {}]
+  });
+
+  expect(newState.categories.length).toBe(2);
+  expect(initialState.categories.length).toBe(1);
+});
