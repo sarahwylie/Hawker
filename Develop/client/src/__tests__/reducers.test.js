33ea68fbc67d4c5d1fac2ddd4eda1697741ddcbe
Nicholas Nolen <nicnolen@ymail.com>
2022-04-18 08:56:44 -0500

    wrote test for UPDATE_CURRENT_CATEGORY action

--- Develop/client/src/__tests__/reducers.test.js
+++ Develop/client/src/__tests__/reducers.test.js
@@ -29,3 +29,13 @@ test('UPDATE_CATEGORIES', () => {
   expect(newState.categories.length).toBe(2);
   expect(initialState.categories.length).toBe(1);
 });
+
+test('UPDATE_CURRENT_CATEGORY', () => {
+  let newState = reducer(initialState, {
+    type: UPDATE_CURRENT_CATEGORY,
+    currentCategory: '2'
+  });
+
+  expect(newState.currentCategory).toBe('2');
+  expect(initialState.currentCategory).toBe('1');
+});
