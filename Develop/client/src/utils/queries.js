da70acbf4bc6aa8670ea2868542faa628b98b015
Nicholas Nolen <nicnolen@ymail.com>
2022-04-18 12:33:51 -0500

    added GlobalState to utils and a category query to queries.js

--- Develop/client/src/utils/queries.js
+++ Develop/client/src/utils/queries.js
@@ -1,5 +1,14 @@
 import { gql } from '@apollo/client';
 
+export const QUERY_CATEGORIES = gql`
+  {
+    categories {
+      _id
+      name
+    }
+  }
+`;
+
 export const QUERY_USER = gql`
   query users {
     users {
