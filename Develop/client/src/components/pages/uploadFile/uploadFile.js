c7a8f674d897336f58fe718dea11078ec589cac4
Nicholas Nolen <nicnolen@ymail.com>
2022-04-20 13:34:47 -0500

    set state to allow image and url to show up when hawking an item

--- Develop/client/src/components/pages/uploadFile/uploadFile.js
+++ Develop/client/src/components/pages/uploadFile/uploadFile.js
@@ -1,35 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function UploadForm() {
+  const [fileInputState, setFileInputState] = useState('');
+  const [previewSource, setPreviewSource] = useState('');
 
   const handleFileChange = async (e) => {
-      let file = e.target.files[0];
-      var formData = new FormData();
+    const file = e.target.files[0];
+    previewFile(file);
+    setFileInputState(e.target.value);
+  };
 
-      formData.append("file", file);
-      formData.append("cloud_name","dkmsnpghg")
-      formData.append("upload_preset", "fjzsl5iq");
-
-      let res = await fetch(
-        "https://api.cloudinary.com/v1_1/dkmsnpghg/image/upload",
-        {
-          method: "post",
-          mode: "cors",
-          body: formData
-        }
-      );
-
-      let json = await res.json();
-    localStorage.setItem('imageurl', `${JSON.stringify(json.secure_url)}`)
-
+  const previewFile = (file) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      setPreviewSource(reader.result);
+    };
   };
 
   return (
     <div>
-      <h1 className='title'>Upload File</h1>
-
-      <input type="file" onChange={handleFileChange} value='' className='btn-primary' />
-
+      <h1 className="title">Upload File</h1>
+      <div>
+        <input type="file" onChange={handleFileChange} value={fileInputState} />
+        {previewSource && (
+          <img src={previewSource} alt="chosen" style={{ width: '100px', height: '100px' }} />
+        )}
+      </div>
     </div>
   );
 }
