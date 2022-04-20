9bf55069a04e7e3f11952a07aaffa852bafcb520
Nicholas Nolen <nicnolen@ymail.com>
2022-04-20 10:11:29 -0500

    got rid of unused variables

--- Develop/client/src/components/SearchList/index.js
+++ Develop/client/src/components/SearchList/index.js
@@ -1,26 +1,12 @@
-import React from 'react'
-import data from '../pages/Homepage/imagesData.json';
+import React from 'react';
 
-function SearchList(props) {
-        //create a new array by filtering the original array
-        const filteredData = data.filter((el) => {
-            //if no input the return the original
-            // if (props.input === '') {
-            //     return el;
-            // }
-            // //return the item which contains the user input
-            // else {
-            //     return el.text.toLowerCase().includes(props.input)
-            // }
-            return el;
-        })
-    return (
-        <ul>
-            {filteredData.map((item) => (
-                <li  className="searchList" key={item.id}>{item.text}</li>
-            ))}
-        </ul>
-    )
+function SearchList() {
+
+  return (
+    <ul>
+      <li className="searchList"></li>
+    </ul>
+  );
 }
 
-export default SearchList
+export default SearchList;
--- Develop/client/src/components/pages/Auto/index.js
+++ Develop/client/src/components/pages/Auto/index.js
@@ -1,23 +1,16 @@
 import React from 'react';
-import { CCard, CCardImage, CCardBody, CCardTitle, CCardText, CButton } from '@coreui/react';
-
-import imagesData from './imagesData.json';
+import { CCard, CCardBody, CButton } from '@coreui/react';
 
 function Auto() {
   return (
     <div>
       <div className="title">Auto</div>
       <div className="itemContainer">
-        {imagesData.map((image, i) => (
-          <CCard key={i}>
-            <CCardImage orientation="top" {...imagesData[i]} alt={imagesData[i].alt} width="100%" />
-            <CCardBody>
-              <CCardTitle>{imagesData[i].alt}</CCardTitle>
-              <CCardText>{imagesData[i].category}</CCardText>
-              <CButton href="#">Check it out!</CButton>
-            </CCardBody>
-          </CCard>
-        ))}
+        <CCard>
+          <CCardBody>
+            <CButton href="#">Check it out!</CButton>
+          </CCardBody>
+        </CCard>
       </div>
     </div>
   );
