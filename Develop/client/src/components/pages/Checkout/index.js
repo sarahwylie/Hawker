869f4f7ac4b61b9bd472488b365e859b82b5faaa
loch <nigelloch@gmail.com>
2022-04-19 20:46:03 -0500

    added item details

--- Develop/client/src/components/pages/Checkout/index.js
+++ Develop/client/src/components/pages/Checkout/index.js
@@ -1,14 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import { useLazyQuery } from '@apollo/client';
-import { QUERY_CHECKOUT } from '../../../utils/queries';
+import { QUERY_CHECKOUT, QUERY_SINGLE_ITEM } from '../../../utils/queries';
 import { loadStripe } from '@stripe/stripe-js';
 import Confirmation from '../Confirmation';
+import { getInclusionDirectives } from '@apollo/client/utilities';
+import { useLocation } from 'react-router-dom'
 
 const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
-const Checkout = () => {
+const Checkout = (product) => {
   const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
+  let location = useLocation()
+  // let itemDetails = location.item
+  // itemInfo = thing.state
+  // itemDetails = itemInfo.image.image.item.category...
+  const details = location.state.itemData.item
+  console.log('something',details)
   const [modalOpen, setModalOpen] = useState(false);
+
   useEffect(() => {
     if (data) {
       stripePromise.then((res) => {
@@ -29,17 +38,20 @@ const Checkout = () => {
   return (
     <div>
       <div>
-      <h1>Below is the modal</h1>
+      <h1>Purchase Confirmation</h1>
+      <h3>Shipping Details</h3>
       <button className='openModalBtn'
       onClick={() => {
         setModalOpen(true);
       }}
       >
-        Ship To
+        Add
       </button>
       {modalOpen && <Confirmation setModalOpen={setModalOpen} /> }
       </div>
       <button onClick={submitCheckout}>Checkout</button>
+      <p>{details.price}</p>
+      <img src={details.image}></img>
     </div>
   );
 };
