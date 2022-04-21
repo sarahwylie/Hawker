2e27c19c2e21e73550d26b120bd831582a5c01b9
JideOgun <JideOgun@github.com>
2022-04-21 07:55:32 -0500

    fixed bug with in server.js that doesn't allow Apollo playground to be queried

--- Develop/client/src/components/pages/Success/success.js
+++ Develop/client/src/components/pages/Success/success.js
@@ -1,8 +1,33 @@
 import React from 'react';
-
+import { useMutation } from '@apollo/client';
+import { ADD_ORDER } from '../../../utils/mutations';
 
 function Success() {
-    return ( <div>Success</div> );
+  const [addOrder] = useMutation(ADD_ORDER);
+
+  const itemData = JSON.parse(localStorage.getItem('itemData'));
+  let itemId = itemData.item._id;
+  console.log(itemData.item._id);
+
+  const saveOrder = () => {
+    const { data } = addOrder({ variables: { id: itemId,
+     } });
+    console.log(data);
+
+    setTimeout(() => {
+      localStorage.removeItem('itemData');
+      window.location.assign('/');
+    }, 5000);
+  };
+
+  return (
+    <div>
+      <h1>Success!</h1>
+      <h2>Thank you for your purchase!</h2>
+      <h2>You will now be redirected to the home page</h2>
+      {saveOrder()}
+    </div>
+  );
 }
 
 export default Success;
