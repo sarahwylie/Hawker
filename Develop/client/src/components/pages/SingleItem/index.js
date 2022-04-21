98155ff3a19e4cea571bf887d78706a84f2e2789
Nicholas Nolen <nicnolen@ymail.com>
2022-04-21 15:42:34 -0500

    updated heroku substrings

--- Develop/client/src/components/pages/SingleItem/index.js
+++ Develop/client/src/components/pages/SingleItem/index.js
@@ -6,7 +6,7 @@ import { CButton } from '@coreui/react';
 
 function SingleItem() {
   // substring number probably will change we stop hosting on Local host
-  let itemId = window.location.href.substring(27);
+  let itemId = window.location.href.substring(46);
   const { data } = useQuery(QUERY_SINGLE_ITEM, { variables: { id: itemId } });
   console.info(data);
 
