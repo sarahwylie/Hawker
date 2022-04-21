c9b435d7dda6ee2ffddc35a67ed89677637ed912
JideOgun <JideOgun@github.com>
2022-04-21 14:39:44 -0500

    updates

--- Develop/client/src/components/pages/Dashboard/orderHistory.js
+++ Develop/client/src/components/pages/Dashboard/orderHistory.js
@@ -15,13 +15,12 @@ function OrderHistory() {
       <div>
         <div>
           {data.user.firstName} {data.user.lastName}'s Order History
-          {/* {console.log(data.user.orders[28].items[0].title)} */}
         </div>
         {data.user.orders.map((e) => (
           <div key={e._id}>
             {' '}
-            {/* title - {e.items[0].title} */}
-            Purchase Date - {e.purchaseDate} orderId - {e._id} 
+            title - {e.items[0].title}
+            Purchase Date - {e.purchaseDate} orderId - {e._id}
           </div>
         ))}
       </div>
@@ -32,8 +31,9 @@ function OrderHistory() {
     <div>
       Order History
       <div>
-        <div className="itemContainer">{data ? getUserOrders() : <div>You have not placed any orders yet...</div>}</div>
-        
+        <div className="itemContainer">
+          {data ? getUserOrders() : <div>You have not placed any orders yet...</div>}
+        </div>
       </div>
     </div>
   );
--- Develop/client/src/components/pages/SingleItem/index.js
+++ Develop/client/src/components/pages/SingleItem/index.js
@@ -22,6 +22,7 @@ function SingleItem() {
       variables: { id: itemId }
     });
     console.log(deleteMutation);
+    window.location.assign('/')
   };
 
 
