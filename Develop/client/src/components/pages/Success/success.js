2163fdec84cae590b2d6c6d1b109d79083455185
JideOgun <JideOgun@github.com>
2022-04-21 11:55:56 -0500

    linting

--- Develop/client/src/components/pages/Success/success.js
+++ Develop/client/src/components/pages/Success/success.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { useMutation } from '@apollo/client';
 import { ADD_ORDER } from '../../../utils/mutations';
 
@@ -7,34 +7,28 @@ function Success() {
 
   const itemData = JSON.parse(localStorage.getItem('itemData'));
   let userId = localStorage.getItem('userId');
-  console.log(userId)
+  console.log(userId);
   let itemId = itemData.item._id;
   console.log(itemData.item._id);
 
   useEffect(() => {
+    const saveOrder = async () => {
+      const data = await addOrder({ variables: { item: itemId, user: userId } });
+      console.log(data);
 
-
-      const saveOrder = async() => {
-    const  data  = await addOrder({ variables: { item: itemId,
-      user: userId
-     } });
-    console.log(data);
-
-    setTimeout(() => {
-      localStorage.removeItem('itemData');
-      window.location.assign('/');
-    }, 5000);
-  };
-  saveOrder();
-  }, [addOrder, itemId, userId])
-
+      setTimeout(() => {
+        localStorage.removeItem('itemData');
+        window.location.assign('/');
+      }, 5000);
+    };
+    saveOrder();
+  }, [addOrder, itemId, userId]);
 
   return (
     <div>
       <h1>Success!</h1>
       <h2>Thank you for your purchase!</h2>
       <h2>You will now be redirected to the home page</h2>
-      
     </div>
   );
 }
